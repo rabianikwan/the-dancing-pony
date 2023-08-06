7013056cf1ea74832d6c131cf427c2ca4f1b9c15
rabianikwan <rabianikwan67@gmail.com>
2023-08-06 23:18:33 +0800

    feat: add docker, install packages, set auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { DishesModule } from './dishes/dishes.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
+import * as process from 'process';
 
 @Module({
-  imports: [AuthModule, DishesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'admin',
+      password: 'admin',
+      database: 'timetracking',
+      entities: [path.join(process.cwd(), 'dist/**/*.entity.js')],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
