b38bb59aa3744497473a7b9043885e22f6ed50b3
rabianikwan <rabianikwan67@gmail.com>
2023-08-09 13:34:15 +0800

    feat: add main modules

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,16 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      message: 'This is a simple example of item returned by your APIs.',
+    };
+  }
+
+  getSecureResource() {
+    return {
+      message:
+        'Access to protected resources granted! This protected resource is displayed when the token is successfully provided.',
+    };
   }
 }
