8d9d9e056ad6c33af5858c3ce92bba526a78eb1a
rabianikwan <rabianikwan67@gmail.com>
2023-08-09 13:33:35 +0800

    feat: add test unit

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,90 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
+import { AccessTokenGuard } from '../src/iam/login/guards/access-token/access-token.guard';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe('App (e2e)', () => {
+  let app;
+  let accessTokenJwt: string;
+  let refreshTokenJwt: string;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideGuard(AccessTokenGuard)
+      .useValue({ canActivate: () => true })
+      .compile();
 
     app = moduleFixture.createNestApplication();
+    app.setGlobalPrefix('api');
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        forbidNonWhitelisted: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('AppController (e2e)', () => {
+    it('should return the follwing message: "This is a simple example of item returned by your APIs." [GET /api]', () => {
+      return request(app.getHttpServer())
+        .get('/api')
+        .expect({
+          message: 'This is a simple example of item returned by your APIs.',
+        })
+        .expect(HttpStatus.OK);
+    });
+
+    describe('should sign in and get a "live" JWT', () => {
+      it('should authenticates user with valid credentials and provides a jwt token', () => {
+        return request(app.getHttpServer())
+          .post('/api/auth/login')
+          .send({
+            email: 'test@example.com',
+            password: 'pass123',
+          })
+          .then(({ body }) => {
+            accessTokenJwt = body.accessToken;
+            refreshTokenJwt = body.refreshToken;
+
+            expect(accessTokenJwt).toMatch(
+              /^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/,
+            );
+
+            expect(refreshTokenJwt).toMatch(
+              /^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/,
+            );
+
+            expect(body).toEqual({
+              accessToken: accessTokenJwt,
+              refreshToken: refreshTokenJwt,
+              user: { name: 'name #1', email: 'test@example.com', id: 1 },
+            });
+
+            expect(HttpStatus.OK);
+          });
+      });
+
+      it('should return the follwing message: "Access to protected resources granted! This protected resource is displayed when the token is successfully provided". - ( endpoint protected ) [GET /api/secure]', () => {
+        return request(app.getHttpServer())
+          .get('/api/secure')
+          .set('Authorization', `Bearer ${accessTokenJwt}`)
+          .expect({
+            message:
+              'Access to protected resources granted! This protected resource is displayed when the token is successfully provided.',
+          });
+      });
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
